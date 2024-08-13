ceaf932e21c08bad42a460f8edff86afacd3685a
9io9 <mario9w@126.com>
2024-08-13 14:36:54 +0800

    cstr test

---
+++ test.c
@@ -0,0 +1,42 @@
+//
+// Created by mario on 2024/8/13.
+//
+
+#include <string.h>
+
+#include "cstr.h"
+
+int main(int argc, char* argv[]) {
+
+    CString* cstr;
+
+    if (cstr_new(sizeof("hello") - 1, "hello", &cstr) != CStrSuccess) {
+        return -1;
+    }
+
+    if (cstr_pushs(sizeof("catmore") - 1, "catmore", cstr) != CStrSuccess) {
+        return -1;
+    }
+
+    char str_buf[32] = { 0 };
+
+    memset(str_buf, 'l', 31);
+
+    if (cstr_strcpy(4, 3, str_buf, CSTR_DEFAULT_CPYFUNC, cstr) != CStrSuccess) {
+        return -1;
+    }
+
+    fprintf(stdout, "%s\n", str_buf);
+
+    if (cstr_pops(5, str_buf, cstr) != CStrSuccess) {
+        return -1;
+    }
+
+    fprintf(stdout, "%s\n", str_buf);
+
+    if (cstr_fprint(stdout, cstr) != CStrSuccess) {
+        return -1;
+    }
+
+    return 0;
+}
